4fde52a70b37d44a9d9d0b31fdd833d6739d2821
Sam Chen <xiaoyangchen123@gmail.com>
2024-04-27 04:16:57 +0000

    initial Gemini App code

---
+++ web/main.js
@@ -0,0 +1,46 @@
+import { streamGemini } from './gemini-api.js';
+
+let form = document.querySelector('form');
+let promptInput = document.querySelector('input[name="prompt"]');
+let output = document.querySelector('.output');
+
+form.onsubmit = async (ev) => {
+  ev.preventDefault();
+  output.textContent = 'Generating...';
+
+  try {
+    // Load the image as a base64 string
+    let imageUrl = form.elements.namedItem('chosen-image').value;
+    let imageBase64 = await fetch(imageUrl)
+      .then(r => r.arrayBuffer())
+      .then(a => base64js.fromByteArray(new Uint8Array(a)));
+
+    // Assemble the prompt by combining the text with the chosen image
+    let contents = [
+      {
+        type: "text",
+        text: promptInput.value,
+      },
+      {
+        type: "image_url",
+        image_url: `data:image/jpeg;base64,${imageBase64}`,
+      },
+    ];
+
+    // Call the gemini-pro-vision model, and get a stream of results
+    let stream = streamGemini({
+      model: 'gemini-pro-vision',
+      contents,
+    });
+
+    // Read from the stream and interpret the output as markdown
+    let buffer = [];
+    let md = new markdownit();
+    for await (let chunk of stream) {
+      buffer.push(chunk);
+      output.innerHTML = md.render(buffer.join(''));
+    }
+  } catch (e) {
+    output.innerHTML += '<hr>' + e;
+  }
+};
